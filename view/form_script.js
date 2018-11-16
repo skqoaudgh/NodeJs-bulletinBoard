c9f64a98747bc6d1667c415e47caef09bcc01c3a
myungho <skqoaudgh2@hanmail.net>
2018-11-16 15:34:53 +0900

    [add] 회원가입 시, 입력 정보에 대한 정규표현식 검사 추가, 형식이 틀릴 시 출력되도록 추가

--- view/form_script.js
+++ view/form_script.js
@@ -2,6 +2,25 @@ $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"},"slow");
  });
  function check() {
-     console.log("asd");
-    return false;
+    var exp_email = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
+    var exp_name = /^[A-za-z0-9]/g;
+
+    var erorr_msg = document.getElementById('error');
+    var email = document.getElementById('reg_email');
+    var name = document.getElementById('reg_id');
+    var password = document.getElementById('reg_pwd');
+
+    if(exp_name.test(name.value) == false) {
+        erorr_msg.innerHTML = '아이디에는 숫자와 영어만 들어갈 수 있습니다.';
+        return false;
+    }
+    else if(password.value.length < 6) {
+        erorr_msg.innerHTML = '비밀번호가 너무 짧습니다.';
+        return false;
+    }
+    else if(exp_email.test(email.value) == false) {
+        erorr_msg.innerHTML = '이메일 형식이 옳바르지 않습니다.';
+        return false;
+    }
+    return true;
 }
